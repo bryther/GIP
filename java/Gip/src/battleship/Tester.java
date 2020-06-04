6dc8d4e98efea05e2192bda37717686dfe107bb2
bryther <57659326+bryther@users.noreply.github.com>
2020-06-04 10:16:54 +0200

    dagelijks werk

--- java/Gip/src/battleship/Tester.java
+++ java/Gip/src/battleship/Tester.java
@@ -10,19 +10,20 @@ public class Tester {
 	 * @author Alec
 	 */
 	public static void main(String[] args) {
-		Fleet test = new Fleet();
+		Board test = new Board(null);
+		test.drawBoard();
+
 		/*
-		 * test.addPB(new Coordinate(6, 6), new Coordinate(6, 7)); test.addSM(new
-		 * Coordinate(1, 3), new Coordinate(2, 3), new Coordinate(3, 3)); test.addDS(new
-		 * Coordinate(8, 3), new Coordinate(8, 4), new Coordinate(8, 5)); test.addBS(new
-		 * Coordinate(3, 5), new Coordinate(3, 6), new Coordinate(3, 7), new
-		 * Coordinate(3, 8)); test.addACC(new Coordinate(3, 1), new Coordinate(4, 1),
-		 * new Coordinate(5, 1), new Coordinate(6, 1), new Coordinate(7, 1));
-		 * test.printer();
+		 * Fleet test = new Fleet(); test.addPB(new Coordinate(6, 6), new Coordinate(6,
+		 * 7)); test.addSM(new Coordinate(1, 3), new Coordinate(2, 3), new Coordinate(3,
+		 * 3)); test.addDS(new Coordinate(8, 3), new Coordinate(8, 4), new Coordinate(8,
+		 * 5)); test.addBS(new Coordinate(3, 5), new Coordinate(3, 6), new Coordinate(3,
+		 * 7), new Coordinate(3, 8)); test.addACC(new Coordinate(3, 1), new
+		 * Coordinate(4, 1), new Coordinate(5, 1), new Coordinate(6, 1), new
+		 * Coordinate(7, 1)); test.printer();
+		 * 
+		 * Fleet enemy = new Fleet(); enemy.SpawnFleet(); enemy.printer();
 		 */
-		Fleet enemy = new Fleet();
-		enemy.SpawnFleet();
-		enemy.printer();
 
 		// System.out.println(coordinantesCOM);
 
